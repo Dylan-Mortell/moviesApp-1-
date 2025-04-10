99a4bf687d02e08ecf2badbb9673fb9abf8c3268
Dylan-Mortell <20093313@mail.wit.ie>
2025-04-10 05:19:31 +0100

    "Refactor: Favourite movie list now stored in a context."

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,8 @@ import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
+import MoviesContextProvider from "./contexts/moviesContext";
+
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -23,17 +25,18 @@ const queryClient = new QueryClient({
     return (
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-         <SiteHeader />      {/* New Header  */}
-
-        <Routes>
+          <SiteHeader />
+            <MoviesContextProvider>
+              <Routes>
             <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<Navigate to="/" />} />
             <Route path="/reviews/:id" element={<MovieReviewPage/>} />
-        </Routes>
-        </BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} />
+            </Routes>
+        </MoviesContextProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
--- src/stories/movieCard.stories.ts
+++ src/stories/movieCard.stories.ts
@@ -3,21 +3,35 @@ import MovieCard from "../components/movieCard";
 import SampleMovie from "./sampleData";
 
 const meta = {
-    title: 'Home Page/MovieCard',
-    component: MovieCard,
+  title: 'Home Page/MovieCard',
+  component: MovieCard,
 } satisfies Meta<typeof MovieCard>;
+
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-export const Basic: Story = {
-    args: SampleMovie
 
+export const Basic: Story = {
+  args: {
+    movie: SampleMovie,
+    selectFavourite: (movieId: number) => {
+      console.log("Favourite clicked for movie ID:", movieId);
+    },
+  },
 };
 Basic.storyName = "Default";
 
-const sampleNoPoster = { ...SampleMovie, poster_path: undefined };
-export const Exceptional: Story = {
-    args: sampleNoPoster
+const sampleNoPoster = {
+  ...SampleMovie,
+  poster_path: undefined,
+};
 
+export const Exceptional: Story = {
+  args: {
+    movie: sampleNoPoster,
+    selectFavourite: (movieId: number) => {
+      console.log("Favourite clicked for movie ID:", movieId);
+    },
+  },
 };
 Exceptional.storyName = "Exception";
