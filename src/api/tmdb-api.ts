5e8fedb54058a8a65992c0a6bd69f3fee1b9f520
Dylan-Mortell <20093313@mail.wit.ie>
2025-04-28 22:30:30 +0100

    Addition of multi-criteria search

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -1,224 +1,174 @@
+const API_KEY = import.meta.env.VITE_TMDB_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+// Basic Movie API
 export const getMovies = (page: number = 1) => {
-  return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=${page}`
-  )
+  return fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&page=${page}`)
     .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Failed to fetch movies. Status: ${response.status}`);
-      }
+      if (!response.ok) throw new Error(`Failed to fetch movies. Status: ${response.status}`);
       return response.json();
     })
-    .catch((error) => {
-      throw error;
-    });
+    .catch((error) => { throw error; });
 };
 
-
-  export const getUpcomingMovies = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-    ).then((response) => {
-      if (!response.ok)
-        throw new Error(`Unable to fetch movies. Response status: ${response.status}`);
+export const getUpcomingMovies = () => {
+  return fetch(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Unable to fetch upcoming movies. Status: ${response.status}`);
       return response.json();
     })
-      .catch((error) => {
-        throw error
-      });
-  };
+    .catch((error) => { throw error; });
+};
 
-  export const getMovieCredits = (id: string) => {
-    return fetch(
-        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-    ).then((response) => {
-      if (!response.ok)
-        throw new Error(`Unable to fetch movies. Response status: ${response.status}`);
+export const getMovie = (id: string | number) => {
+  return fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Failed to get movie data. Status: ${response.status}`);
       return response.json();
     })
-      .catch((error) => {
-        throw error
-      });
-  };
+    .catch((error) => { throw error; });
+};
 
-  export const getSimilarMovies = (Id: string | number) => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${Id}/similar?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-    ).then((response) => {
-      if (!response.ok)
-        throw new Error(`Unable to fetch movies. Response status: ${response.status}`);
+export const getMovieCredits = (id: string | number) => {
+  return fetch(`${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}&language=en-US`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Unable to fetch movie credits. Status: ${response.status}`);
       return response.json();
     })
-      .catch((error) => {
-        throw error
-      });
-  };
+    .catch((error) => { throw error; });
+};
 
-  export const getTvSeries = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/tv/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
-    ) .then((response) => {
-        if (!response.ok)
-          throw new Error(`Unable to fetch TV series. Response status: ${response.status}`);
-        return response.json();
-      })
-      .catch((error) => {
-        throw error;
-      });
-  };
-
-  export const getTvSeriesDetails = (id: string | number) => {
-    return fetch(
-      `https://api.themoviedb.org/3/tv/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch TV series details. Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        throw error;
-      });
-  };
-
-  export const getSimilarTvSeries = (id: string | number) => {
-    return fetch(
-      `https://api.themoviedb.org/3/tv/${id}/similar?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch TV series details. Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        throw error;
-      });
-  };
-
-  export const getTvSeriesCredits = (id: string | number) => {
-    return fetch(
-      `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch TV series details. Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        throw error;
-      });
-  };
-
-  export const getFantasyMovies = async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&with_genres=14&language=en-US`
-    );
-  
-    if (!response.ok) {
-      throw new Error(`Failed to fetch fantasy movies. Status: ${response.status}`);
-    }
-  
-    const data = await response.json();
-    return data.results; 
-  };
-  
-
-  
-  export const getPopularMovies = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-    ).then((response) => {
-      if (!response.ok)
-        throw new Error(`Unable to fetch movies. Response status: ${response.status}`);
+export const getSimilarMovies = (id: string | number) => {
+  return fetch(`${BASE_URL}/movie/${id}/similar?api_key=${API_KEY}&language=en-US&page=1`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Unable to fetch similar movies. Status: ${response.status}`);
       return response.json();
     })
-      .catch((error) => {
-        throw error
-      });
-  };
+    .catch((error) => { throw error; });
+};
 
-
-  
-  export const getMovie = async (page: number = 1) => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    ).then((response) => {
-      if (!response.ok) {
-        throw new Error(`Failed to get movie data. Response status: ${response.status}`);
-      }
+export const getPopularMovies = () => {
+  return fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Unable to fetch popular movies. Status: ${response.status}`);
       return response.json();
     })
-    .catch((error) => {
-      throw error
-   });
-  }; 
-  
-  export const getGenres = () => {
-    return fetch(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=" + import.meta.env.VITE_TMDB_KEY + "&language=en-US"
-    ).then( (response) => {
-      if (!response.ok)
-        throw new Error(`Unable to fetch genres. Response status: ${response.status}`);
+    .catch((error) => { throw error; });
+};
+
+export const getFantasyMovies = async () => {
+  const response = await fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=14&language=en-US`);
+  if (!response.ok) throw new Error(`Failed to fetch fantasy movies. Status: ${response.status}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const getGenres = () => {
+  return fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Unable to fetch genres. Status: ${response.status}`);
       return response.json();
     })
-    .catch((error) => {
-      throw error
-   });
-  };
-  
+    .catch((error) => { throw error; });
+};
 
-  export const getMovieImages = (id: string | number) => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    ).then((response) => {
-      if (!response.ok) {
-        throw new Error("failed to fetch images");
-      }
+export const getMovieImages = (id: string | number) => {
+  return fetch(`${BASE_URL}/movie/${id}/images?api_key=${API_KEY}`)
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed to fetch movie images");
       return response.json();
-    }).then((json) => json.posters)
-      .catch((error) => {
-        throw error
-      });
-  };
-  
+    })
+    .then((json) => json.posters)
+    .catch((error) => { throw error; });
+};
 
-  export const getTvSeriesImages = (id: string | number) => {
-    return fetch(
-      `https://api.themoviedb.org/3/mtv/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    ).then((response) => {
-      if (!response.ok) {
-        throw new Error("failed to fetch images");
-      }
+export const getMovieReviews = (id: string | number) => {
+  return fetch(`${BASE_URL}/movie/${id}/reviews?api_key=${API_KEY}`)
+    .then((res) => res.json())
+    .then((json) => json.results);
+};
+
+// TV Series
+export const getTvSeries = () => {
+  return fetch(`${BASE_URL}/tv/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Unable to fetch TV series. Status: ${response.status}`);
       return response.json();
-    }).then((json) => json.posters)
-      .catch((error) => {
-        throw error
-      });
-  };
+    })
+    .catch((error) => { throw error; });
+};
 
+export const getTvSeriesDetails = (id: string | number) => {
+  return fetch(`${BASE_URL}/tv/${id}?api_key=${API_KEY}&language=en-US`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Failed to fetch TV series details. Status: ${response.status}`);
+      return response.json();
+    })
+    .catch((error) => { throw error; });
+};
 
-export const getMovieReviews = (id: string | number) => { //movie id can be string or number
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        // console.log(json.results);
-        return json.results;
-      });
-  };
+export const getSimilarTvSeries = (id: string | number) => {
+  return fetch(`${BASE_URL}/tv/${id}/similar?api_key=${API_KEY}&language=en-US`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Failed to fetch similar TV series. Status: ${response.status}`);
+      return response.json();
+    })
+    .catch((error) => { throw error; });
+};
 
-  export const getTvSeriesReviews = (id: string | number) => { //movie id can be string or number
-    return fetch(
-      `https://api.themoviedb.org/3/tv/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        // console.log(json.results);
-        return json.results;
-      });
-  };
+export const getTvSeriesCredits = (id: string | number) => {
+  return fetch(`${BASE_URL}/tv/${id}/credits?api_key=${API_KEY}&language=en-US`)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Failed to fetch TV series credits. Status: ${response.status}`);
+      return response.json();
+    })
+    .catch((error) => { throw error; });
+};
 
+export const getTvSeriesImages = (id: string | number) => {
+  return fetch(`${BASE_URL}/tv/${id}/images?api_key=${API_KEY}`)
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed to fetch TV images");
+      return response.json();
+    })
+    .then((json) => json.posters)
+    .catch((error) => { throw error; });
+};
 
+export const getTvSeriesReviews = (id: string | number) => {
+  return fetch(`${BASE_URL}/tv/${id}/reviews?api_key=${API_KEY}`)
+    .then((res) => res.json())
+    .then((json) => json.results);
+};
+
+//  Multi-Criteria Movie Filtering (server-side)
+export const fetchFilteredMovies = async ({ queryKey }: any) => {
+  const [_key, filters] = queryKey as [string, { name: string; value: string }[]];
+  const filterMap = Object.fromEntries(filters.map(f => [f.name, f.value]));
+
+  const baseUrl = filterMap.title?.trim()
+    ? `${BASE_URL}/search/movie`
+    : `${BASE_URL}/discover/movie`;
+
+  const params = new URLSearchParams({
+    api_key: API_KEY,
+    language: 'en-US',
+    page: '1',
+  });
+
+  if (filterMap.title && baseUrl.includes("search")) {
+    params.append("query", filterMap.title);
+  }
+
+  if (filterMap.genre && filterMap.genre !== "0") {
+    params.append("with_genres", filterMap.genre);
+  }
+
+  if (filterMap.premium_genre && filterMap.premium_genre !== "0") {
+    params.append("with_genres", filterMap.premium_genre);
+  }
+
+  const response = await fetch(`${baseUrl}?${params.toString()}`);
+  if (!response.ok) throw new Error("Failed to fetch filtered movies");
+  return response.json();
+};
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { getMovies } from "../api/tmdb-api";
 import useFiltering from "../hooks/useFiltering";
 import MovieFilterUI, { titleFilter, genreFilter } from "../components/movieFilterUI";
 import PageTemplate from "../components/templateMovieListPage";
 import { BaseMovieProps, DiscoverMovies } from "../types/interfaces";
 import Spinner from "../components/spinner";
 import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import { fetchFilteredMovies } from "../api/tmdb-api";
 
+// Filtering logic
 const titleFiltering = {
   name: "title",
   value: "",
@@ -16,67 +17,53 @@ const titleFiltering = {
 
 const genreFiltering = {
   name: "genre",
-  value: "0", 
+  value: "0",
 };
 
-// Premium filters (available only to authenticated users)
 const premiumGenreFiltering = {
   name: "premium_genre",
-  value: "0",  // Default genre for premium
-  condition: genreFilter, 
+  value: "0",
+  condition: genreFilter,
 };
 
 const HomePage: React.FC = () => {
-  const isAuthenticated = localStorage.getItem("token") !== null; // Check if the user is authenticated
-  const { data, error, isLoading, isError } = useQuery<DiscoverMovies, Error>("discover", getMovies);
+  const isAuthenticated = localStorage.getItem("token") !== null;
 
-  // Conditionally apply filters based on whether the user is authenticated
   const filters = isAuthenticated
-    ? [titleFiltering, genreFiltering, premiumGenreFiltering]  
-    : [titleFiltering, genreFiltering];  
+    ? [titleFiltering, genreFiltering, premiumGenreFiltering]
+    : [titleFiltering, genreFiltering];
 
-  const { filterValues, setFilterValues, filterFunction } = useFiltering(filters);
+  const { filterValues, setFilterValues } = useFiltering(filters);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
-
-  if (isError) {
-    return <h1>{error.message}</h1>;
-  }
-
-  // Filter the movies using filterFunction from useFiltering hook
-  const movies = data ? data.results : [];
-  const displayedMovies = filterFunction(movies);
-
+  const { data, error, isLoading, isError } = useQuery(
+    ["discover", filterValues],
+    fetchFilteredMovies
+  );
+  
   const changeFilterValues = (type: string, value: string) => {
-    const changedFilter = { name: type, value: value };
-    const updatedFilterSet =
-      type === "title"
-        ? [changedFilter, filterValues[1], filterValues[2]]  
-        : type === "premium_genre"
-        ? [filterValues[0], filterValues[1], changedFilter]
-        : [filterValues[0], changedFilter, filterValues[2]]; 
-
-    setFilterValues(updatedFilterSet);
+    const updated = filterValues.map(f =>
+      f.name === type ? { ...f, value } : f
+    );
+    setFilterValues(updated);
   };
+
+  if (isLoading) return <Spinner />;
+  if (isError) return <h1>{error.message}</h1>;
+
+  const movies = data?.results ?? [];
 
   return (
     <>
       <PageTemplate
         title="Discover Movies"
-        movies={displayedMovies}  // Display filtered movies
-        action={(movie: BaseMovieProps) => {
-          return <AddToFavouritesIcon {...movie} />;
-        }}
+        movies={movies}
+        action={(movie: BaseMovieProps) => <AddToFavouritesIcon {...movie} />}
       />
-      
       <MovieFilterUI
         onFilterValuesChange={changeFilterValues}
-        titleFilter={filterValues[0].value}
-        genreFilter={filterValues[1].value}
-        // Pass the premium genre filter value if available
-        premiumGenreFilter={isAuthenticated ? filterValues[2].value : undefined}
+        titleFilter={filterValues.find(f => f.name === "title")?.value || ""}
+        genreFilter={filterValues.find(f => f.name === "genre")?.value || "0"}
+        premiumGenreFilter={isAuthenticated ? filterValues.find(f => f.name === "premium_genre")?.value : undefined}
       />
     </>
   );
